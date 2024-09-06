523d2402d6a375bb936cadeba7c9898e2cd22b3f
Alexander Bukharov <buharovsasha@yandex.ru>
2024-09-06 23:36:47 +0700

    add task-runner-service

---
+++ services/task-runner-service/src/main/java/com/bukharov/scheduler/task_runner_service/TaskRunnerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bukharov.scheduler.task_runner_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskRunnerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskRunnerServiceApplication.class, args);
+	}
+
+}
---
+++ services/task-runner-service/src/test/java/com/bukharov/scheduler/task_runner_service/TaskRunnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bukharov.scheduler.task_runner_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskRunnerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

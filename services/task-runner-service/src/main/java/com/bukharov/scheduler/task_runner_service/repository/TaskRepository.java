ee8a8361ea3be7ee433142354ba53ebbf8203525
Alexander Bukharov <buharovsasha@yandex.ru>
2024-10-16 09:59:46 +0700

    load tasks in runner

--- services/task-runner-service/src/main/java/com/bukharov/scheduler/task_runner_service/repository/TaskRepository.java
+++ services/task-runner-service/src/main/java/com/bukharov/scheduler/task_runner_service/repository/TaskRepository.java
@@ -1,6 +1,11 @@
 package com.bukharov.scheduler.task_runner_service.repository;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 public interface TaskRepository extends CrudRepository<TaskEntity, Long> {
+
+	List<TaskEntity> findByNextExecutionTimeLessThan(ZonedDateTime time);
 }
--- services/task-runner-service/src/main/java/com/bukharov/scheduler/task_runner_service/runner/TaskRunnerImpl.java
+++ services/task-runner-service/src/main/java/com/bukharov/scheduler/task_runner_service/runner/TaskRunnerImpl.java
@@ -1,14 +1,25 @@
 package com.bukharov.scheduler.task_runner_service.runner;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.bukharov.scheduler.task_runner_service.repository.TaskEntity;
+import com.bukharov.scheduler.task_runner_service.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 
 public class TaskRunnerImpl implements TaskRunner {
 
 	public static final int ONE_MINUTE_IN_MILLISECONDS = 60000;
 
+	@Autowired
+	private TaskRepository taskRepository;
+
 	@Scheduled(fixedRate = ONE_MINUTE_IN_MILLISECONDS)
 	public void scheduleFixedRateTask() {
-		System.out.println(
-				"Fixed rate task - " + System.currentTimeMillis() / 1000);
+
+		List<TaskEntity> tasks = taskRepository.findByNextExecutionTimeLessThan(ZonedDateTime.now());
+		System.out.println(String.format("Tasks for run: [%s]", tasks.stream().map(TaskEntity::name).collect(Collectors.joining())));
 	}
 }

4476ec14a87f29408fcd4fdc08bdb43e3fae66f1
Alexander Bukharov <buharovsasha@yandex.ru>
2025-12-27 09:39:50 +0700

    Refactor TaskController to use @RequestMapping for better routing

--- services/api-service/src/main/java/com/bukharov/scheduler/api_service/api/TaskController.java
+++ services/api-service/src/main/java/com/bukharov/scheduler/api_service/api/TaskController.java
@@ -4,10 +4,12 @@ import com.bukharov.scheduler.api_service.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController("/tasks")
+@RestController
+@RequestMapping("/tasks")
 public class TaskController {
 
 	@Autowired

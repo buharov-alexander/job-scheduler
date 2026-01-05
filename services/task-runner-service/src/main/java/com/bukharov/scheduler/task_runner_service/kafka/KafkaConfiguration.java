372cd7edd8e9fed732440d1b0166c7ee82fa123c
Alexander Bukharov <buharovsasha@yandex.ru>
2026-01-05 22:41:10 +0700

    Add Kafka configuration for topic creation and admin management

--- services/task-runner-service/src/main/java/com/bukharov/scheduler/task_runner_service/kafka/KafkaConfiguration.java
+++ services/task-runner-service/src/main/java/com/bukharov/scheduler/task_runner_service/kafka/KafkaConfiguration.java
@@ -3,12 +3,15 @@ package com.bukharov.scheduler.task_runner_service.kafka;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -18,6 +21,8 @@ public class KafkaConfiguration {
 
 	@Value("${kafka.address}")
 	private String kafkaAddress;
+	@Value("${kafka.topic.name}")
+	private String kafkaTopicName;
 
 	@Bean
 	public ProducerFactory<String, TaskMessage> producerFactory() {
@@ -33,4 +38,17 @@ public class KafkaConfiguration {
 	public KafkaTemplate<String, TaskMessage> kafkaTemplate() {
 		return new KafkaTemplate<>(producerFactory());
 	}
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaAddress);
+
+        return new KafkaAdmin(config);
+    }
+
+	@Bean
+    public NewTopic topic() {
+        return new NewTopic(kafkaTopicName, 2, (short) 1);
+    }
 }
